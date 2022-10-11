a488a5ca0ab3bf14280789fa0fb7848ae1922236
Alceu-wv <alceu.wv@gmail.com>
2022-10-11 13:16:13 -0300

    first commit

---
+++ lib/src/login/login_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatelessWidget {
+  static const routeName = '/';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(children: const [
+      Text("BaskApp"),
+    ]));
+  }
+}
